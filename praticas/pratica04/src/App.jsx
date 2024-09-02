7f52b02704c3ab7e40488fef8451ed2d62c84a36
Jose Carlos <jcsantos.souza1988@gmail.com>
2024-09-02 20:13:57 -0300

    Adicionando arquivos da pratica04

---
+++ praticas/pratica04/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Header />
+      <p>Este é um parágrafo em JSX!</p>
+    </div>
+  );
+}
+
+export default App;
